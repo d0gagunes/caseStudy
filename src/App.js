21adb814f29e5fdadc80e5bce3490dfa955eac90
Doga Gunes <73892482+d0gagunes@users.noreply.github.com>
2021-04-20 14:02:36 +0300

    Project Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import RectArea from "./RectArea";
+import React, { useState } from "react";
+import {
+  defaultHeaderText,
+  defaultTitleText,
+  defaultContentText,
+} from "./Default";
+const App = () => {
+  const [headerTextUp, setHeaderTextUp] = useState(defaultHeaderText);
+  const [titleTextUp, setTitleTextUp] = useState(defaultTitleText); //state
+  const [contentTextUp, setContentTextUp] = useState(defaultContentText);
 
-function App() {
+  const [headerTextDown, setHeaderTextDown] = useState(defaultHeaderText);
+  const [titleTextDown, setTitleTextDown] = useState(defaultTitleText);
+  const [contentTextDown, setContentTextDown] = useState(defaultContentText);
+
+  const sendFunc = () => {
+    setHeaderTextDown(headerTextUp);
+    setTitleTextDown(titleTextUp);
+    setContentTextDown(contentTextUp);
+    setHeaderTextUp(defaultHeaderText);
+    setTitleTextUp(defaultTitleText);
+    setContentTextUp(defaultContentText);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RectArea
+        headerText={headerTextUp}
+        setHeaderText={setHeaderTextUp}
+        titleText={titleTextUp}
+        setTitleText={setTitleTextUp}
+        contentText={contentTextUp}
+        setContentText={setContentTextUp}
+        readonly={false}
+        onSendMethod={sendFunc}
+      />
+      <div
+        style={{ height: "10px", backgroundColor: "#c4c4c4" }}
+        className="border"
+      />
+      <RectArea
+        headerText={headerTextDown}
+        titleText={titleTextDown}
+        contentText={contentTextDown}
+        readonly={true}
+      />
+    </>
   );
-}
+};
 
 export default App;
